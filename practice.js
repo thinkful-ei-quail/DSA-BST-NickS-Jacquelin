4e413e1fcf1121033a6358d0ea6d107ad6da666e
Jackie Abert <jax.abert@gmail.com>
2020-09-29 12:26:26 -0400

    while loop

--- practice.js
+++ practice.js
@@ -2,18 +2,33 @@ const BinarySearchTree = require('./BinarySearchTree')
 
 function main() {
     const BST = new BinarySearchTree()
-    BST.insert(3,3)
-    BST.insert(1,1)
-    BST.insert(4,4)
-    BST.insert(6,6)
-    BST.insert(9,9)
-    BST.insert(2,2)
-    BST.insert(5,5)
-    BST.insert(7,7)
-    
-    console.log(BST)
-    console.log(BST.find(9))
+    // BST.insert(3,3)
+    // BST.insert(1,1)
+    // BST.insert(4,4)
+    // BST.insert(6,6)
+    // BST.insert(9,9)
+    // BST.insert(2,2)
+    // BST.insert(5,5)
+    // BST.insert(7,7)
+    // console.log(BST)
+    // console.log(BST.find(9))
 
+    BST.insert('E')
+    BST.insert('A')
+    BST.insert('S')
+    BST.insert('Y')
+    BST.insert('Q')
+    BST.insert('U')
+    BST.insert('E')
+    BST.insert('S')
+    BST.insert('T')
+    BST.insert('I')
+    BST.insert('O')
+    BST.insert('N')
+    // while(1){
+    console.log(BST)
+// }
 }
 main()
+
 
