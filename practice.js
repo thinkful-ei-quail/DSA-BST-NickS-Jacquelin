a8863dd3dc06d2f357c3289063bb0327bf390d23
Jackie Abert <jax.abert@gmail.com>
2020-09-29 12:50:32 -0400

    mystery function

--- practice.js
+++ practice.js
@@ -2,33 +2,40 @@ const BinarySearchTree = require('./BinarySearchTree')
 
 function main() {
     const BST = new BinarySearchTree()
-    // BST.insert(3,3)
-    // BST.insert(1,1)
-    // BST.insert(4,4)
-    // BST.insert(6,6)
-    // BST.insert(9,9)
-    // BST.insert(2,2)
-    // BST.insert(5,5)
-    // BST.insert(7,7)
+    BST.insert(3,3)
+    BST.insert(1,1)
+    BST.insert(4,4)
+    BST.insert(6,6)
+    BST.insert(9,9)
+    BST.insert(2,2)
+    BST.insert(5,5)
+    BST.insert(7,7)
     // console.log(BST)
     // console.log(BST.find(9))
 
-    BST.insert('E')
-    BST.insert('A')
-    BST.insert('S')
-    BST.insert('Y')
-    BST.insert('Q')
-    BST.insert('U')
-    BST.insert('E')
-    BST.insert('S')
-    BST.insert('T')
-    BST.insert('I')
-    BST.insert('O')
-    BST.insert('N')
+    // BST.insert('E')
+    // BST.insert('A')
+    // BST.insert('S')
+    // BST.insert('Y')
+    // BST.insert('Q')
+    // BST.insert('U')
+    // BST.insert('E')
+    // BST.insert('S')
+    // BST.insert('T')
+    // BST.insert('I')
+    // BST.insert('O')
+    // BST.insert('N')
     // while(1){
-    console.log(BST)
+    // console.log(BST)
 // }
+return BST
 }
-main()
+BST = main()
 
-
+function tree(t){
+    if(!t){
+        return 0;
+    }
+    return tree(t.left) + t.value + tree(t.right)
+}
+console.log(tree(BST))
