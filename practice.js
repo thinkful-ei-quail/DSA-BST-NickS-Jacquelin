b58ec9ff37a3cbe77ed3da962f854630968b0e22
Jackie Abert <jax.abert@gmail.com>
2020-09-29 12:00:43 -0400

    bst class

---
+++ practice.js
@@ -0,0 +1,19 @@
+const BinarySearchTree = require('./BinarySearchTree')
+
+function main() {
+    const BST = new BinarySearchTree()
+    BST.insert(3,3)
+    BST.insert(1,1)
+    BST.insert(4,4)
+    BST.insert(6,6)
+    BST.insert(9,9)
+    BST.insert(2,2)
+    BST.insert(5,5)
+    BST.insert(7,7)
+    
+    console.log(BST)
+    console.log(BST.find(9))
+
+}
+main()
+
